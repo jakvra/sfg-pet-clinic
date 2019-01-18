449e4890bbd1c0e428bb3d9829a9ff00aa65aa41
Jakub Vrana <jvrana@monetplus.cz>
2019-01-18 14:09:30 +0100

    Fixed Broken links. Closes #46

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/config/SpringSecConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgpetclinic.config;
+
+//import org.springframework.security.config.annotation.ObjectPostProcessor;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecConfig {
+
+}
+//public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    public void setObjectPostProcessor(ObjectPostProcessor<Object> objectPostProcessor) {
+//        super.setObjectPostProcessor(objectPostProcessor);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//    }
+//}
